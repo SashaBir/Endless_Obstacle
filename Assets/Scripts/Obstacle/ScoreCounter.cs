5342bfdc9bbde16fc9b63010e4cdec7d8b99c776
Александр Бирюков <112517746+SanyaLorik@users.noreply.github.com>
2023-05-13 11:48:42 +0300

    add ScoreCounter

---
+++ Assets/Scripts/Obstacle/ScoreCounter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using UnityEngine;
+
+[RequireComponent(typeof(Collider))]
+[RequireComponent(typeof(Rigidbody))]
+public class ScoreCounter : MonoBehaviour
+{
+    public event Action<int> OnScoreUpdated;
+    public event Action<int> OnScoreGotten;
+
+    private int _score = 0;
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.TryGetComponent(out Obstacle obstacle) == false)
+            return;
+
+        _score += obstacle.Score;
+
+        OnScoreUpdated?.Invoke(_score);
+        OnScoreGotten?.Invoke(obstacle.Score);
+
+        Debug.Log($"Score: {_score}");
+    }
+}
